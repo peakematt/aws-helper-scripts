1764562218ea622543ff199085f4cf8992c65b99
Matt Boehlke <matthew.boehlke@gmail.com>
2022-01-19 20:05:24 -0500

    Clean Script

--- ec2/attach_security_group_multiple_instances.py
+++ ec2/attach_security_group_multiple_instances.py
@@ -1,5 +1,4 @@
 import boto3
-import json
 
 ec2_client = boto3.client('ec2')
 ec2_resource = boto3.resource("ec2")
@@ -12,26 +11,14 @@ group_to_attach = "sg-078aa25a637c8189f"
 
 for reserv in response["Reservations"]:
     for instance in reserv["Instances"]:
-        instances.append(instance)
+        if instance["State"]["Name"] == "terminated":
+            continue
 
+        i = ec2_resource.Instance(instance["InstanceId"])
+        sg_ids = list()
+        for group in instance['SecurityGroups']:
+            sg_ids.append(group["GroupId"])
 
-for instance in instances:
-    if instance["State"]["Name"] == "terminated":
-        continue
-    instance_id = instance["InstanceId"]
-    security_groups = instance['SecurityGroups']
-    sg_ids = list()
-    for group in security_groups:
-        sg_ids.append(group["GroupId"])
+        sg_ids.append(group_to_attach)
 
-    # print("--------------")
-    #print("Instance: {}".format(instance_id))
-    # for group in security_groups:
-    #    print("Group: {}".format(group["GroupId"]))
-    # print("--------------")
-
-    sg_ids.append(group_to_attach)
-
-    i = ec2_resource.Instance(instance_id)
-
-    i.modify_attribute(Groups=sg_ids)
+        i.modify_attribute(Groups=sg_ids)
