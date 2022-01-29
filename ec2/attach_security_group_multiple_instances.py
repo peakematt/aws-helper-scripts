d9886e8604ad8b4770ebe891f01525a2ca1f2004
Matt Boehlke <matthew.boehlke@gmail.com>
2022-01-29 09:39:16 -0500

    boilerplate

--- ec2/attach_security_group_multiple_instances.py
+++ ec2/attach_security_group_multiple_instances.py
@@ -1,33 +1,38 @@
 import boto3
-ec2_client = boto3.client('ec2')
-ec2_resource = boto3.resource("ec2")
 
-response = ec2_client.describe_instances()
-desired_region = "us-east-1"
 
-instances = list()
+def main():
+    ec2_client = boto3.client('ec2')
+    ec2_resource = boto3.resource("ec2")
 
-group_to_attach = "sg-somethin"
+    response = ec2_client.describe_instances()
+    desired_region = "us-east-1"
 
-for reserv in response["Reservations"]:
-    for instance in reserv["Instances"]:
-        if instance["State"]["Name"] == "terminated":
-            continue
+    group_to_attach = "sg-somethin"
 
-        if desired_region not in instance["Placement"]["AvailabilityZone"]:
-            continue
+    for reserv in response["Reservations"]:
+        for instance in reserv["Instances"]:
+            if instance["State"]["Name"] == "terminated":
+                continue
 
-        i = ec2_resource.Instance(instance["InstanceId"])
-        sg_ids = list()
-        for group in instance['SecurityGroups']:
-            sg_ids.append(group["GroupId"])
+            if desired_region not in instance["Placement"]["AvailabilityZone"]:
+                continue
 
-        sg_ids.append(group_to_attach)
+            i = ec2_resource.Instance(instance["InstanceId"])
+            sg_ids = list()
+            for group in instance['SecurityGroups']:
+                sg_ids.append(group["GroupId"])
 
-        try:
-            i.modify_attribute(Groups=sg_ids)
-            print("Successfully updated instance: {}. YAY!".format(
-                instance['InstanceId']))
-        except:
-            print("skipping instance: {}. Encountered an error.".format(
-                instance['InstanceId']))
+            sg_ids.append(group_to_attach)
+
+            try:
+                i.modify_attribute(Groups=sg_ids)
+                print("Successfully updated instance: {}. YAY!".format(
+                    instance['InstanceId']))
+            except:
+                print("skipping instance: {}. Encountered an error.".format(
+                    instance['InstanceId']))
+
+
+if __name__ == "__main__":
+    main()
